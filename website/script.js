7b88b9db4b78760881d42563889d215705670d37
Katrine <kitj91@gmail.com>
2015-10-11 18:13:33 +0200

    add mock-ups, add P2-text, create form.

--- website/script.js
+++ website/script.js
@@ -53,7 +53,39 @@ $(function () {
   });
 
   // Last inn footer inn i footer elementet
-  $("#footer").load("html/footer.html");
+  $("#footer").load("html/footer.html", function () {
+    // Ventet til nav har lastet inn
+
+    // Lytt på alle "a" elementer so
+    // har en "href" adresse
+    $('a[href]').click(function(event) {
+      // Stopp klikket fra å navigere oss bort
+      console.log("Caught click from el:", $(this)[0]);
+      event.preventDefault();
+
+      // Finn addressen som var lenket til
+      var page = $(this).attr("href");
+      console.log("Trying to navigate to: ", page)
+      
+      if (page === "#") {
+         return false;
+      }
+
+      window.history.pushState({},"", page);
+
+      // Last inn den adressen inn i main
+      // elementet
+      $("#main").load("html/" + page);
+
+      //Viser i navBaren hvilken side en er pa
+      //$("#navBar").
+
+      // For a laste inn ankermeny
+      $("#ankerMeny").load("html/"+ page + "#ankerMeny")
+
+      return false;
+    });
+  });
 
 
 });
