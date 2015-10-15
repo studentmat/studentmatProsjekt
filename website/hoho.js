190409a3d06bcba9497d0525c19d167838384e22
Aasne <Aasne.n@hotmail.com>
2015-10-15 14:59:40 +0200

    Jobbet med js til tips

--- website/hoho.js
+++ website/hoho.js
@@ -1,8 +1,28 @@
 // Sier at visOppskrift er en global funskjon
-visDiv = function (divId){
+visDiv = function (divId, smallOrBig){
 	console.log(divId);
-  $("#"+divId+" .inndelingDiv").toggleClass("active");
-  console.log("makes #"+divId+" > inndelingDiv active");
-  //Skjuler den lille visningen av oppskriften
-  $("#"+divId + " .divHeading").hide();
+  console.log(smallOrBig);
+  // Vis det er divHeadingClosed funksjonen kjøres på
+  if (smallOrBig == "divSmall") {
+    $("#"+divId+" .divBig").toggleClass("active");
+    $("#"+divId + " .divBig").removeClass("hidden");
+
+    
+    console.log("makes #"+divId+" > divBig active");
+    //Skjuler den lille visningen av oppskriften
+    $("#"+divId + " .divSmall").removeClass("active");
+    $("#"+divId + " .divSmall").toggleClass("hidden");
+  }
+
+  //Hvis det er divHeadingOpen
+  else if (smallOrBig == "divBig") {
+    $("#"+divId+" .divSmall").toggleClass("active");
+    $("#"+divId + " .divSmall").removeClass("hidden");
+
+    console.log("makes #"+divId+" > divSmall active");
+    //Skjuler den lille visningen av oppskriften
+    $("#"+divId + " .divBig").removeClass('active');
+     $("#"+divId + " .divBig").toggleClass('hidden');
+  }
+
 }
