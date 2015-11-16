42d4b968adc57d87194c39e0506c8d848197570a
Dora Oline <doraoline@gmail.com>
2015-11-16 22:34:14 +0100

    Fjernet mobildesign-linje fra js

--- website/script.js
+++ website/script.js
@@ -133,8 +133,8 @@ var updateNavbar = function(page) {
 
 //Holder navbaren fiksert i toppen ved å legge til classen fixed om det er lengre til toppen enn headers hoyde
 $(window).bind('scroll', function () {
-    // Hvis vi har scrollet lengre ned enn headerens høyde, og det ikke er en mobiltelefon vi er på
-    if ($(window).scrollTop() > $("#header").outerHeight() && screen.width >= 769) {
+    // Hvis vi har scrollet lengre ned enn headerens høyde
+    if ($(window).scrollTop() > $("#header").outerHeight()) {
       $('#navBar').addClass('fixed');
     } else {
       $('#navBar').removeClass('fixed');
